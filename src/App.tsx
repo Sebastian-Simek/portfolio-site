28c0aaae875d5cdd910f47d9f507349728fde269
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-11-02 15:24:19 -0700

    Added tailwind stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
-import Home from "./components/Home/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
 import Layout from "./components/Layout";
-import Projects from "./components/Projects/Projects";
+import Projects from "./components/Projects";
 
 export default function App() {
   return (
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
-    <nav>
+    <nav className="relative container m-3">
       <NavLink to="/">Home</NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/projects">Projects</NavLink>
